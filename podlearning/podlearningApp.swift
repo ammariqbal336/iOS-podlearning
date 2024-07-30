4f14a330b23852789fa4fd9901cc8abcef0794bb
ammar <muhammad.ammar@appiskey.com>
2024-07-30 21:55:24 +0500

    Initial Commit

---
+++ podlearning/podlearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  podlearningApp.swift
+//  podlearning
+//
+//  Created by mac on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct podlearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
