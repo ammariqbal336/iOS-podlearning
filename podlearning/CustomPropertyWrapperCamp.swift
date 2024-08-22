c48062172a410ff82ded92a935e926972fec81b1
ammar <muhammad.ammar@appiskey.com>
2024-08-22 22:31:31 +0500

    custom property

--- podlearning/CustomPropertyWrapperCamp.swift
+++ podlearning/CustomPropertyWrapperCamp.swift
@@ -7,29 +7,57 @@
 
 import SwiftUI
 
-struct FileManagerProperty {
-    var title : String = "Title"
+extension FileManager {
     
-    private var path : URL {
+    static func documentPath(key : String) -> URL {
         FileManager.default
             .urls(for: .documentDirectory, in: .userDomainMask)
             .first!
-            .appending(path: "custom_title.txt")
+            .appending(path: "\(key).txt")
     }
+}
+
+@propertyWrapper
+struct FileManagerProperty : DynamicProperty {
+  @State private var title : String
+    let key: String
     
-    mutating func load() {
-        do {
-            title = try String(contentsOf: path,encoding: .utf8)
-        }catch {
-            
+    var wrappedValue : String {
+        get {
+            title
+        }
+        nonmutating set {
+            save(newValue: newValue)
         }
     }
     
-    
-    
-   mutating func save(newValue: String) {
+    var projectedValue : Binding<String> {
+        
+        Binding {
+            wrappedValue
+        } set: { newValue in
+            wrappedValue = newValue
+        }
+
+    }
+
+    init(wrappedValue: String, _ key: String) {
+        self.key = key
         do {
-            try newValue.write(to: path, atomically: false, encoding: .utf8)
+            title = try String(contentsOf: FileManager.documentPath(key: key),encoding: .utf8)
+            
+            print("Success")
+            print("\(title)")
+        }catch {
+            title = wrappedValue
+            print("Failed")
+        }
+    }
+    
+    func save(newValue: String) {
+        do {
+            try newValue.write(to: FileManager.documentPath(key: key), atomically: false, encoding: .utf8)
+            print(NSHomeDirectory())
             title = newValue
             print("Success")
         } catch {
@@ -40,26 +68,29 @@ struct FileManagerProperty {
 
 struct CustomPropertyWrapperCamp: View {
     
-    @State var fileManagerProperty = FileManagerProperty()
+    @FileManagerProperty("custom_text1") private var title : String = "Starting Value"
+    @FileManagerProperty("custom_text2") private var title2 : String = "Starting Value"
+   //  var fileManagerProperty = FileManagerProperty()
   //  @State private var title: String = "Title Info"
     var body: some View {
         VStack {
-            Text(fileManagerProperty.title)
+            Text(title)
                 .font(.largeTitle)
-            
+            Text(title2)
+                .font(.largeTitle)
+            ChildPropertyView(title: $title)
             Button("Click 1") {
-                fileManagerProperty.save(newValue: "Click 1")
+                title = "Click 1"
+               // fileManagerProperty.save(newValue: "Click 1")
                 //setTitle(newValue: "Click1")
             }
             
             Button("Click 2") {
-                fileManagerProperty.save(newValue: "Click 2")
+                title2 = "Click 2"
+                //fileManagerProperty.save(newValue: "Click 2")
                 //setTitle(newValue: "Click2")
             }
             
-        }
-        .onAppear {
-            fileManagerProperty.load()
         }
     }
     
@@ -84,6 +115,15 @@ struct CustomPropertyWrapperCamp: View {
 //            print("Failed")
 //        }
 //    }
+}
+
+struct ChildPropertyView : View {
+    @Binding var title: String
+    var body: some View {
+        Button(title) {
+            title = "Clicked"
+        }
+    }
 }
 
 #Preview {
