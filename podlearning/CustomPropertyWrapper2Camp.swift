2144248809c43d2c02e4e4ff8fd4646c7bf5f890
ammar <muhammad.ammar@appiskey.com>
2024-08-24 19:57:09 +0500

    property wrapper

--- podlearning/CustomPropertyWrapper2Camp.swift
+++ podlearning/CustomPropertyWrapper2Camp.swift
@@ -32,6 +32,29 @@ struct UserProfile :Codable {
     var premium: Bool
 }
 
+struct FileManagerValue {
+    static let shared = FileManagerValue()
+    private init() {}
+    
+    let userProfile = FileManagerKeypath(key: "user_profile", type: UserProfile.self)
+}
+
+struct FileManagerKeypath<T: Codable> {
+    let key: String
+    let type: T.Type
+    
+}
+
+struct CustomProjectedValue<T:Codable> {
+    
+    let binding : Binding<T?>
+    let publisher : CurrentValueSubject<T? ,Never>
+    
+    var stream : AsyncPublisher<CurrentValueSubject<T?, Never>> {
+       publisher.values
+    }
+}
+
 @propertyWrapper
 struct FileManagerCustomProperty<T: Codable> : DynamicProperty {
   @State private var value : T?
@@ -56,18 +79,37 @@ struct FileManagerCustomProperty<T: Codable> : DynamicProperty {
 
     }
 
-    init(wrappedValue: T?, _ key: String) {
+    init(_ key: String) {
         self.key = key
         do {
             let url = FileManager.documentPath(key: key)
             let data = try Data(contentsOf: url)
             
             let object = try JSONDecoder().decode(T.self, from: data)
-            value = object
+            _value = State(wrappedValue: object)
             print("Success")
            // print("\(title)")
         }catch {
-            value = wrappedValue
+            _value = State(wrappedValue: nil)
+            print("Failed")
+        }
+    }
+    
+    init(_ key: KeyPath<FileManagerValue, FileManagerKeypath<T>>) {
+        let keyPath = FileManagerValue.shared[keyPath: key]
+        let key = keyPath.key
+        
+        self.key = key
+        do {
+            let url = FileManager.documentPath(key: key)
+            let data = try Data(contentsOf: url)
+            
+            let object = try JSONDecoder().decode(T.self, from: data)
+            _value = State(wrappedValue: object)
+            print("Success")
+           // print("\(title)")
+        }catch {
+            _value = State(wrappedValue: wrappedValue)
             print("Failed")
         }
     }
@@ -85,19 +127,129 @@ struct FileManagerCustomProperty<T: Codable> : DynamicProperty {
 }
 
 struct CustomPropertyWrapper2Camp: View {
-    @FileManagerCustomProperty ("user_profile") private var user: UserProfile? = nil
+  //  @FileManagerCustomProperty ("user_profile") private var user: UserProfile?
+   // @FileManagerCustomProperty (\.userProfile) private var user
+    @FileManagerCustomStreamProperty (\.userProfile) private var user
     
     @Capitalized var title: String = "hello world!"
     var body: some View {
         VStack {
             Button(user?.name ?? "no value") {
-                user = UserProfile(name: "Ammar", age: 29, premium: true)
+                user = UserProfile(name: "Merger", age: 29, premium: true)
+            }
+            ChildCustomProperty(user: $user.binding)
+        }
+        .onReceive($user.publisher, perform: { newValue in
+            print("New Value Stream \(newValue)")
+        })
+        .task {
+            for await newValue in $user.stream {
+                print("New Value Stream \(newValue)")
             }
         }
         
     }
 }
 
+struct ChildCustomProperty : View {
+    @Binding var user : UserProfile?
+    var body: some View {
+        Button(user?.name ?? "no value") {
+            user = UserProfile(name: "Final", age: 29, premium: true)
+        }
+    }
+}
+
 #Preview {
     CustomPropertyWrapper2Camp()
+}
+
+import Combine
+
+@propertyWrapper
+struct FileManagerCustomStreamProperty<T: Codable> : DynamicProperty {
+  @State private var value : T?
+    let key: String
+    private let publisher: CurrentValueSubject<T?, Never>
+    
+    var wrappedValue : T? {
+        get {
+            value
+        }
+        nonmutating set {
+            save(newValue: newValue)
+        }
+    }
+    var projectedValue: CustomProjectedValue<T> {
+        CustomProjectedValue(
+            binding: Binding {
+                         wrappedValue
+                    } set: { newValue in
+                        wrappedValue = newValue
+                    }
+             , publisher: publisher)
+    }
+//    var projectedValue : Binding<T?> {
+//        
+//        Binding {
+//            wrappedValue
+//        } set: { newValue in
+//            wrappedValue = newValue
+//        }
+//
+//    }
+//    var projectedValue : CurrentValueSubject<T?,Never> {
+//        publisher
+//    }
+
+    init(_ key: String) {
+        self.key = key
+        do {
+            let url = FileManager.documentPath(key: key)
+            let data = try Data(contentsOf: url)
+            
+            let object = try JSONDecoder().decode(T.self, from: data)
+            _value = State(wrappedValue: object)
+            publisher = CurrentValueSubject(object)
+            print("Success")
+           // print("\(title)")
+        }catch {
+            _value = State(wrappedValue: nil)
+            publisher = CurrentValueSubject(nil)
+            print("Failed")
+        }
+    }
+    
+    init(_ key: KeyPath<FileManagerValue, FileManagerKeypath<T>>) {
+        let keyPath = FileManagerValue.shared[keyPath: key]
+        let key = keyPath.key
+        
+        self.key = key
+        do {
+            let url = FileManager.documentPath(key: key)
+            let data = try Data(contentsOf: url)
+            
+            let object = try JSONDecoder().decode(T.self, from: data)
+            _value = State(wrappedValue: object)
+            publisher = CurrentValueSubject(object)
+            print("Success")
+           // print("\(title)")
+        }catch {
+            _value = State(wrappedValue: nil)
+            publisher = CurrentValueSubject(nil)
+            print("Failed")
+        }
+    }
+    
+    func save(newValue: T?) {
+        do {
+            let data = try JSONEncoder().encode(newValue)
+            try data.write(to: FileManager.documentPath(key: key))
+            value = newValue
+            publisher.send(newValue)
+            print("Success")
+        } catch {
+            print("Failed")
+        }
+    }
 }
